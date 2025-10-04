9e58a80ffbfe31735a384991ab20963b5f15898c
Somnath Nandi <somnathnandi368@gmail.com>
2025-10-04 19:06:15 +0530

    adding some usage and creating structure of the core functions

--- src/core.rs
+++ src/core.rs
@@ -1,4 +1,26 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::OpenOptions};
+use std::io::Write;
+
+
+pub const WORKING_DIR: &str = ".config/envset/";
+
+pub fn get_config_dir() -> String {
+    let home_dir = std::env::var("HOME").expect("Unable to get HOME directory");
+    let config_dir_path = std::path::Path::new(&home_dir).join(WORKING_DIR);
+    config_dir_path.to_str().unwrap().to_string()
+}
+
+pub fn init_setup() {
+    let home_dir = std::env::var("HOME").expect("Unable to get HOME directory");
+    let config_dir_path = std::path::Path::new(&home_dir).join(WORKING_DIR);
+
+    if !config_dir_path.exists() {
+        std::fs::create_dir_all(&config_dir_path).expect("Unable to create config directory");
+    }
+
+    // create bash_profile file if not exists
+    // create fish_profile file if not exists
+}
 
 
 #[derive(Debug)]
@@ -32,9 +54,29 @@ pub fn get_shell() -> Shell {
 }
 
 
-pub fn config_bash_env() { todo!()}
-pub fn config_zsh_env() { todo!()}
-pub fn config_fish_env() { todo!()}
+fn write_to_file(file_path: &std::path::Path, content: &str) {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(file_path).unwrap();
+    writeln!(file, "{}", content).unwrap();
+    file.flush().unwrap();
+
+}
+
+pub fn config_bash_env(env_name: String, env_value: String) { 
+    let bash_profile_path = std::path::Path::new(get_config_dir().as_str()).join("bash_profile");
+
+    if env_name.trim() == "PATH" {
+        let content = format!("export {}=\"$PATH:{}\"", env_name, env_value);
+        write_to_file(&bash_profile_path, content.as_str());
+    } else {
+        let content = format!("export {}={}", env_name, env_value);
+        write_to_file(&bash_profile_path, content.as_str());
+    }
+}
+pub fn config_zsh_env(env_name: String, env_value: String) { todo!()}
+pub fn config_fish_env(env_name: String, env_value: String) { todo!()}
 
 
 /// Adds env to the current shell. which is determined by $SHELL env variable.
@@ -44,9 +86,9 @@ pub fn add_env(env_name: String, env_value: String) {
 
     match shell {
         Shell::UNKOWN(shell_path) => println!("Unable to add to '{}'", shell_path),
-        Shell::BASH => config_bash_env(),
-        Shell::FISH => config_fish_env(),
-        Shell::ZSH => config_zsh_env(),
+        Shell::BASH => config_bash_env(env_name, env_value),
+        Shell::FISH => config_fish_env(env_name, env_value),
+        Shell::ZSH => config_zsh_env(env_name, env_value),
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use crate::core::add_env;
+use crate::core::{add_env, init_setup};
 
 mod core;
 
 
 
 fn usage() {
-
+    println!("Usage: envset <ENV_NAME> <ENV_VALUE>");
+    println!("\n\nSets or Edit an environment variable in the current shell session.");
+    println!("If the variable already exists, its value will be updated.");
+    println!("\nExample:");
+    println!("  envset PATH ~/mybin");
+    println!("  envset MY_VAR some_value");
+    println!("\nif no arguments are provided, the env variable will be deleted");
+    println!("Note: For PATH variable, it will not delete the whole PATH variable, only remove the not exist paths");
+    println!("Example: envset PATH");
 }
 
 
@@ -14,9 +22,19 @@ fn main() {
     
     let args: Vec<String> = std::env::args().collect();
 
-    let env_name = args.get(0).unwrap().clone();
-    let env_value = args.get(0).unwrap().clone();
+    if args.len() < 2 {
+        usage();
+        return;
+    }
 
+    let env_name = args.get(1).unwrap().clone();
+    let env_value = args.get(2).unwrap().clone();
+
+    init_setup();
+    
+
+
+    println!("env_name: {}, env_value: {}", env_name, env_value);
 
     add_env(env_name, env_value);
 
