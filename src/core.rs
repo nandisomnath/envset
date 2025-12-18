3b0a5aaa9f7c30b1e77c05dcac8e1f6a18a27f02
Somnath Nandi <somnathnandi368@gmail.com>
2025-12-19 02:15:35 +0530

    changing function signatures

--- src/core.rs
+++ src/core.rs
@@ -3,36 +3,7 @@ use std::io::Write;
 use std::path::Path;
 use std::{collections::HashMap, fs::OpenOptions};
 
-pub const WORKING_DIR: &str = ".config/envset/";
-
-pub fn get_config_dir() -> String {
-    let home_dir = std::env::var("HOME").expect("Unable to get HOME directory");
-    let config_dir_path = std::path::Path::new(&home_dir).join(WORKING_DIR);
-    config_dir_path.to_str().unwrap().to_string()
-}
-
-pub fn init_setup() {
-    let home_dir = std::env::var("HOME").expect("Unable to get HOME directory");
-    let config_dir_path = Path::new(&home_dir).join(WORKING_DIR);
-
-    if !config_dir_path.exists() {
-        std::fs::create_dir_all(&config_dir_path).expect("Unable to create config directory");
-    }
-
-    // create bash_profile file if not exists
-
-    let bash_profile = Path::new(&config_dir_path).join("bash_profile");
-    File::create(&bash_profile).expect("Unable to create the bash_profile.");
-    let bashrc_path = Path::new(&home_dir).join(".bashrc");
-    let content = format!("source {}", bash_profile.as_os_str().to_str().unwrap());
-    write_to_file(&bashrc_path, content.as_str());
-
-    // create fish_profile file if not exists
-    // not need to add the file in tha config.fish
-    let fish_path = ".config/fish/conf.d/envset.fish";
-    let fishconf_path = Path::new(&home_dir).join(fish_path);
-    File::create(&fishconf_path).expect("Unable to create envset.fish file");
-}
+//pub const WORKING_DIR: &str = ".config/envset/";
 
 #[derive(Debug)]
 pub enum ShellOptions {
@@ -52,14 +23,14 @@ pub fn get_shell() -> ShellOptions {
         .clone();
 
     if shell_path.contains("fish") {
-        return Shell::FISH;
+        return ShellOptions::FISH;
     } else if shell_path.contains("bash") {
-        return Shell::BASH;
+        return ShellOptions::BASH;
     } else if shell_path.contains("zsh") {
-        return Shell::ZSH;
+        return ShellOptions::ZSH;
     }
 
-    return Shell::UNKOWN(shell_path);
+    return ShellOptions::UNKOWN(shell_path);
 }
 
 fn write_to_file(file_path: &std::path::Path, content: &str) {
@@ -72,7 +43,7 @@ fn write_to_file(file_path: &std::path::Path, content: &str) {
     file.flush().unwrap();
 }
 
-pub fn config_bash_env(env_name: String, env_value: String) {
+pub fn config_bash_env(env_value: String) {
     let bash_profile_path = Path::new(get_config_dir().as_str()).join("bash_profile");
     println!("{:?}", bash_profile_path);
     if env_name.trim() == "PATH" {
@@ -87,7 +58,7 @@ pub fn config_bash_env(env_name: String, env_value: String) {
 /// fish uses set to set variables. set -gx variable value
 /// unlike bash related shells use export.
 /// set program only sets for current shell or child shell otherwise not parmanent
-pub fn config_fish_env(env_name: String, env_value: String) {
+pub fn config_fish_env(env_value: String) {
     let home_dir = std::env::var("HOME").expect("Unable to get HOME directory");
     let fish_path = ".config/fish/conf.d/envset.fish";
     let fishconf_path = Path::new(&home_dir).join(fish_path);
@@ -101,7 +72,7 @@ pub fn config_fish_env(env_name: String, env_value: String) {
     }
 }
 
-pub fn config_zsh_env(env_name: String, env_value: String) {
+pub fn config_zsh_env(env_value: String) {
     let profile_path = Path::new(get_config_dir().as_str()).join("zsh_profile");
 
     if env_name.trim() == "PATH" {
@@ -114,14 +85,14 @@ pub fn config_zsh_env(env_name: String, env_value: String) {
 }
 
 /// Adds env to the current shell. which is determined by $SHELL env variable.
-pub fn add_env(env_name: String, env_value: String) {
+pub fn add_env(env_value: String) {
     let shell = get_shell();
     println!("{:?}", shell);
 
     match shell {
-        Shell::UNKOWN(shell_path) => println!("Unable to add to '{}'", shell_path),
-        Shell::BASH => config_bash_env(env_name, env_value),
-        Shell::FISH => config_fish_env(env_name, env_value),
-        Shell::ZSH => config_zsh_env(env_name, env_value),
+        ShellOptions::UNKOWN(shell_path) => println!("Unable to add to '{}'", shell_path),
+        ShellOptions::BASH => config_bash_env(env_value),
+        ShellOptions::FISH => config_fish_env(env_value),
+        ShellOptions::ZSH => config_zsh_env(env_value),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,33 @@ mod core;
 mod shell;
 
 fn usage() {
-    println!("Usage: envset <ENV_NAME> <ENV_VALUE>");
-    println!("\n\nSets or Edit an environment variable in the current shell session.");
-    println!("If the variable already exists, its value will be updated.");
+    println!("Usage: envset <PATH_VALUE>");
+    println!("\n\nSets or Edit an path variable for the current seleted shell.");
+    println!("It will update or add or modify only added shell using this program");
     println!("\nExample:");
-    println!("  envset PATH ~/mybin");
-    println!("  envset MY_VAR some_value");
-    println!("\nif no arguments are provided, the env variable will be deleted");
-    println!(
-        "Note: For PATH variable, it will not delete the whole PATH variable, only remove the not exist paths"
-    );
-    println!("Example: envset PATH");
+    println!("  envset ~/mybin");
+    //println!("  envset MY_VAR some_value");
+    //println!("\nif no arguments are provided, the env variable will be deleted");
+    //println!(
+    //  "Note: For PATH variable, it will not delete the whole PATH variable, only remove the not exist paths"
+    //);
+    //println!("Example: envset PATH");
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() < 2 {
+    if args.len() < 1 {
         usage();
         return;
     }
 
-    let env_name = args.get(1).unwrap().clone();
-    let env_value = args.get(2).unwrap().clone();
+    let env_value = args.get(1).unwrap().clone();
+    //let env_value = args.get(2).unwrap().clone();
 
     init_setup();
 
-    println!("env_name: {}, env_value: {}", env_name, env_value);
+    //println!("env_name: {}, env_value: {}", env_name, env_value);
 
-    add_env(env_name, env_value);
+    add_env(env_value);
 }
