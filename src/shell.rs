8714f962fabc8fcd4742d8a124f20790ae33931c
Somnath Nandi <somnathnandi368@gmail.com>
2025-12-19 01:37:44 +0530

    adding new shell stream

--- src/shell.rs
+++ src/shell.rs
@@ -19,8 +19,17 @@ impl Shell {
 
     /// This function setup shell to use envset
     /// This function return a error if failed to setup.
-    pub fn init_setup(&self) -> Result<_, String> {}
+    pub fn init_setup(&self) -> Result<(), String> {
+        Ok(())
+    }
 
     /// Returns shell config of the shells
-    pub fn configs() -> HashMap<ShellOptions, Self> {}
+    /// How to make configs for this.
+    /// ShellOptions as a key, return a new Shell instance
+    /// Every new shell config you have to config it.
+    pub fn from(sop: ShellOptions) -> Self {
+        let mut map = HashMap::new();
+
+        return map;
+    }
 }
